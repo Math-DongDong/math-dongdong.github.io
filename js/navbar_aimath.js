b396aa0f54a668bf95e0678159da02800fd815d8
DongDong <jjes0107@gmail.com>
2026-07-18 00:51:12 +0900

    네비바 수정

--- js/navbar_aimath.js
+++ js/navbar_aimath.js
@@ -14,15 +14,15 @@
         // 'pages' 폴더가 경로에 존재하는 경우 (하위 페이지)
         const depth = pathSegments.length - pagesIndex;
         rootPath = '../'.repeat(depth);
-        basePath = rootPath + 'pages/schoolautomation/';
+        basePath = rootPath + 'pages/aimath/';
     } else {
         // 루트 페이지인 경우
         rootPath = './';
-        basePath = './pages/schoolautomation/';
+        basePath = './pages/aimath/';
     }
 
     const navbarHtml = `
-    <!-- 상단 네비게이션 바 (업무자동화 전용) -->
+    <!-- 상단 네비게이션 바 (인공지능 수학 전용) -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom sticky-top py-2 shadow-sm">
         <div class="container-fluid px-3">
             <a class="navbar-brand d-flex align-items-center fw-bold" href="${rootPath}index.html">
@@ -35,16 +35,14 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-lg-3">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAutomation" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            업무자동화
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAiMath" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            인공지능 수학
                         </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownAutomation">
-                            <!-- 업무자동화 소속 파일 목록 -->
-                            <li><a class="dropdown-item" href="${basePath}appsheet.html">학생관리 앱</a></li>
-                            <li><a class="dropdown-item" href="${basePath}travel_expense_report.html">여비정산 신청서</a></li>
-                            
-                            <!-- 추후 파일이 추가되면 아래와 같이 한 줄씩 복사해서 넣으세요 -->
-                            <!-- <li><a class="dropdown-item" href="\${basePath}새로운파일.html">새로운 메뉴</a></li> -->
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownAiMath">
+                            <!-- 인공지능 수학 소속 파일 목록 -->
+                            <li><a class="dropdown-item" href="${basePath}resolution.html">🖼️ 이미지 해상도</a></li>
+                            <li><a class="dropdown-item" href="${basePath}grayscale.html">⚫ 흑백 이미지</a></li>
+                            <li><a class="dropdown-item" href="${basePath}color.html">🎨 컬러 이미지</a></li>
                         </ul>
                     </li>
                 </ul>
@@ -53,12 +51,17 @@
     </nav>
     `;
 
-    // 스크립트 태그 위치에 HTML 삽입
-    const scriptTag = document.currentScript;
-    if (scriptTag) {
-        scriptTag.insertAdjacentHTML('beforebegin', navbarHtml);
+    // navbar-container 요소가 있으면 거기에 삽입, 없으면 스크립트 위치에 삽입
+    const container = document.getElementById('navbar-container');
+    if (container) {
+        container.innerHTML = navbarHtml;
     } else {
-        document.write(navbarHtml);
+        const scriptTag = document.currentScript;
+        if (scriptTag) {
+            scriptTag.insertAdjacentHTML('beforebegin', navbarHtml);
+        } else {
+            document.write(navbarHtml);
+        }
     }
 
     // 현재 페이지 활성화(파란색 글씨 강조) 표시 로직
