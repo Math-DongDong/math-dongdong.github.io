180cb39cd0db27a294cfdfd56c139466618bd851
DongDong <jjes0107@gmail.com>
2026-09-02 22:30:18 +0900

    네비게이션 메뉴 수정

--- js/mathday-navbar.js
+++ js/mathday-navbar.js
@@ -17,7 +17,6 @@
     { file: 'stopwatch.html', label: '31.4초 멈춰라!', icon: 'bi-stopwatch' },
     { file: 'slider.html', label: 'π×10 맞추기!', icon: 'bi-sliders' },
     { file: 'circle.html', label: '완벽한 원 그리기', icon: 'bi-circle' },
-    { file: 'voronoi.html', label: '보로노이', icon: 'bi-diagram-3' }
   ];
 
   const currentFile = (window.location.pathname.split('/').pop() || 'index.html').toLowerCase();
@@ -33,26 +32,26 @@
 
   const navbarHtml =
     '<nav class="navbar navbar-expand-lg mathday-navbar sticky-top">' +
-      '<div class="container-fluid px-3 px-lg-4">' +
-        '<a class="navbar-brand d-flex align-items-center gap-2" href="index.html">' +
-          '<span class="brand-pi">π</span><span>Math Day!</span>' +
-        '</a>' +
-        '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" ' +
-          'data-bs-target="#mathdayNavMenu" aria-controls="mathdayNavMenu" ' +
-          'aria-expanded="false" aria-label="메뉴 열기">' +
-          '<span class="navbar-toggler-icon"></span>' +
-        '</button>' +
-        '<div class="collapse navbar-collapse" id="mathdayNavMenu">' +
-          '<ul class="navbar-nav ms-auto align-items-lg-center">' +
-            menuHtml +
-            '<li class="nav-item nav-divider d-none d-lg-block"></li>' +
-            '<li class="nav-item">' +
-              '<a class="nav-link nav-home" href="' + rootPath + 'index.html">' +
-              '<i class="bi bi-box-arrow-up-right me-1"></i>메인으로</a>' +
-            '</li>' +
-          '</ul>' +
-        '</div>' +
-      '</div>' +
+    '<div class="container-fluid px-3 px-lg-4">' +
+    '<a class="navbar-brand d-flex align-items-center gap-2" href="index.html">' +
+    '<span class="brand-pi">π</span><span>Math Day!</span>' +
+    '</a>' +
+    '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" ' +
+    'data-bs-target="#mathdayNavMenu" aria-controls="mathdayNavMenu" ' +
+    'aria-expanded="false" aria-label="메뉴 열기">' +
+    '<span class="navbar-toggler-icon"></span>' +
+    '</button>' +
+    '<div class="collapse navbar-collapse" id="mathdayNavMenu">' +
+    '<ul class="navbar-nav ms-auto align-items-lg-center">' +
+    menuHtml +
+    '<li class="nav-item nav-divider d-none d-lg-block"></li>' +
+    '<li class="nav-item">' +
+    '<a class="nav-link nav-home" href="' + rootPath + 'index.html">' +
+    '<i class="bi bi-box-arrow-up-right me-1"></i>메인으로</a>' +
+    '</li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
     '</nav>';
 
   currentScript.insertAdjacentHTML('afterend', navbarHtml);
