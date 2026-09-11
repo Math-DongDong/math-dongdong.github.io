4d44bb5965fddf4d23f3b16489f9222408944fad
DongDong <jjes0107@gmail.com>
2026-09-12 00:26:42 +0900

    네비게이션바 수정

--- js/navbar.js
+++ js/navbar.js
@@ -89,8 +89,8 @@
                     "items": [
                         { "name": "여비정산 신청서", "path": "pages/automation/work/travel_expense_report.html" },
                         { "name": "PDF 편집 도구", "path": "pages/automation/work/pdfedit.html" },
-                        { "name": "엑셀 이미지 추출기", "path": "pages/automation/work/xlimagedownloader.html" },
-                        { "name": "GIF 편집 도구", "path": "pages/automation/work/gifedit.html" }
+                        { "name": "GIF 편집 도구", "path": "pages/automation/work/gifedit.html" },
+                        { "name": "엑셀 이미지 추출기", "path": "pages/automation/work/xlimagedownloader.html" }
                     ]
                 },
                 {
